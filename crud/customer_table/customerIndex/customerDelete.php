9b08e10f701e1c6a93c99344338faabd47e0662c
motlj <joshuadamotl@gmail.com>
2016-02-23 22:45:00 -0600

    tried to fix error

--- crud/customer_table/customerIndex/customerDelete.php
+++ crud/customer_table/customerIndex/customerDelete.php
@@ -6,7 +6,7 @@
         $id = $_POST['id'];
         $pdo = Database::connect();
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "DELETE * FROM customer WHERE id = ?";
+        $sql = "DELETE * FROM customer WHERE id = $id";
         $q = $pdo->prepare($sql);
         //$q->execute(array($id));
         //Database::disconnect();
