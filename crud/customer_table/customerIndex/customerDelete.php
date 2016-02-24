f59bd7f3fa60bc8ecd4b705bf93dd12d94c43c79
motlj <joshuadamotl@gmail.com>
2016-02-23 19:31:53 -0600

    created customerIndex folder with nick for improved functionality of customer table

---
+++ crud/customer_table/customerIndex/customerDelete.php
@@ -0,0 +1,14 @@
+<?php
+ 
+    require_once '../database.php';
+ 
+    if ( !empty($_POST['id']) && isset($_POST['id'])) {
+        $id = $_POST['id'];
+        $pdo = Database::connect();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "DELETE * FROM customer WHERE id=?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($id));
+        Database::disconnect();
+        header("Location: indexwithcreateandupdate.php");
+    }
---
+++ crud/customer_table/customerIndex/customerUpdate.php
@@ -0,0 +1,35 @@
+<?php
+ 
+    require_once '../database.php';
+ 
+    if ( !empty($_POST)) {
+
+      // keep track post values
+      $id = $_POST['id'];
+      $name = $_POST['first_name'];
+      $last_name = $_POST['last_name'];
+      $birthdate = $_POST['dob'];
+      $phone_number = $_POST['phone'];
+      $email_address = $_POST['email'];
+      $user_name = $_POST['username'];
+         
+      function valid($varname){
+        return ( !empty($varname) && isset($varname) );
+      }
+
+      if (!valid($name) || !valid($last_name) || !valid($birthdate) || !valid($user_name) || !valid($phone_number)) {
+        header("Location: indexwithcreateandupdate.php");
+      } else if (!valid($email_address)) {
+        header("Location: indexwithcreateandupdate.php");
+      } else if ( !filter_var($email_address,FILTER_VALIDATE_EMAIL) ) {
+        header("Location: indexwithcreateandupdate.php");
+      }
+
+      $pdo = Database::connect();
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $sql = "UPDATE customer SET name=?,last_name=?,birthdate=?,phone_number=?,email_address=?,user_name=? WHERE id=?";
+      $q = $pdo->prepare($sql);
+      $q->execute(array($name,$last_name,$birthdate,$phone_number,$email_address,$user_name));
+      Database::disconnect();
+      header("Location: indexwithcreateandupdate.php");
+    }
